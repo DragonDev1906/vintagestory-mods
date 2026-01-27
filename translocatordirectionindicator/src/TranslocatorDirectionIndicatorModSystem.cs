8e566910d28e5bb19c159d2b4621349ea9a63e2b
Zokora <8270201+DragonDev1906@users.noreply.github.com>
2026-01-27 19:23:52 +0100

    Remove useless comment

--- translocatordirectionindicator/src/TranslocatorDirectionIndicatorModSystem.cs
+++ translocatordirectionindicator/src/TranslocatorDirectionIndicatorModSystem.cs
@@ -10,8 +10,6 @@ namespace TranslocatorDirectionIndicator;
 
 public class TranslocatorDirectionIndicatorModSystem : ModSystem
 {
-    // Called on server and client
-    // Useful for registering block/entity classes on both sides
     public override void Start(ICoreAPI api)
     {
         base.Start(api);
